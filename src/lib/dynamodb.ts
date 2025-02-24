4ebacffd9c95882ccb8704d017253cd96d27d60b
Marsel <marsel.stefic@gmail.com>
2025-02-24 10:37:31 +0100

    fix: defined variable types

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -4,8 +4,8 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION || "eu-central-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
 

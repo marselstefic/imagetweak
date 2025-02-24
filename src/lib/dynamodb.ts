94f82e77a5029a26747aae70b80f9bf8579bedc1
Marsel <marsel.stefic@gmail.com>
2025-02-24 10:53:21 +0100

    fix: dynamoDBClient for vercel deployment

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -3,7 +3,7 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
   region: process.env.AWS_REGION,

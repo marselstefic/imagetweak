1a4a1639dcdc3843e04c6cb379471bf20eff050a
Marsel <marsel.stefic@gmail.com>
2025-02-24 09:52:10 +0100

    fix: changed dynamodb.ts variables

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -1,7 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION || "eu-central-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
 const dynamoDb = DynamoDBDocumentClient.from(client);
 
